1436e7ff271207386fdb76ee8ce738d26085fcd3
Toleratedskink <158307705+Toleratedskink@users.noreply.github.com>
2024-11-11 22:36:39 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -154,14 +154,3 @@ function deleteAssignment(index) {
 
 // Initial render of the calendar
 renderCalendar(currentMonth, currentYear);
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.log('Service Worker registration failed:', error);
-      });
-  }
-  
